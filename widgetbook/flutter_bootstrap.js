30f8692b209faff70c497b2937e60d4c4a446632
ocpipun <ocpipun@gmail.com>
2026-09-20 09:13:50 +0300

    Add theme picker (3 palettes, default blue) and theme studio

--- widgetbook/flutter_bootstrap.js
+++ widgetbook/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"06a2e2a110089dff50fe635cffd2a61e1b24fb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2724392417" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2617037150" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
